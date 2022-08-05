a7c5136cd04a5ec1f53bb1ca5bf57929956c59f3
Santiago Soñora <santiagochinas@hotmail.com>
2022-08-05 00:42:46 -0300

    first

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return jsonify({'Time': time.strftime("%Y-%m-%d %H:%M:%S")})
+    return jsonify({'message': 'Hello from Kubernetes', 'time': time.strftime("%Y-%m-%d %H:%M:%S")})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
