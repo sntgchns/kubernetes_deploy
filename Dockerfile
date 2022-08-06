b91db0188cb0dc347a58d23763e779e45c0dbb79
Santiago Soñora <santiagochinas@hotmail.com>
2022-08-06 02:02:33 -0300

    test manifests

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,4 @@ RUN pip install -r requirements.txt
 CMD ["python", "/app/app.py"]
 
 # docker build -t flask-kubernetes .
+# docker run -d -p 5000:5000 flask-kubernetes
--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello_world():
     return jsonify({'message': 'Hello from Kubernetes', 'time': time.strftime("%Y-%m-%d %H:%M:%S")})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
